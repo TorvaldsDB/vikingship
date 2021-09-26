fe3589c6a0b92419981c2c9061ae11482b97efeb
Torvalds Du <torvalds@ekohe.com>
2021-09-26 23:01:21 +0800

    Basic Menu Construct

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem>cool link 2</MenuItem>
+          <MenuItem>cool link 3</MenuItem>
+        </Menu>
         <Button
           autoFocus
           className="custom"
