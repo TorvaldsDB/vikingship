38f731b51f40ea889253edee609c73a7bebe4a24
Torvalds Du <torvalds@ekohe.com>
2021-09-26 23:51:34 +0800

    Use Contenxt to implement menu behavors

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import classNames from 'classnames';
 
 type MenuNode = 'horizontal' | 'vertical';
+type SelectCallback = (selectedIndex: number) => void;
 
 export interface MenuProps {
   defaultIndex?: number;
   className?: string;
   mode?: MenuNode;
   style?: React.CSSProperties;
-  onSelect?: (selectedIndex: number) => void;
+  onSelect?: SelectCallback;
 }
+
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({ index: 0 });
 
 const Menu: React.FC<MenuProps> = (props) => {
   const { defaultIndex, className, mode, style, onSelect, children } = props;
+  const [currentActive, setActive] = useState(defaultIndex);
   const classes = classNames('viking-menu', className, {
     'menu-vertical': mode === 'vertical',
   });
+  const handleClick = (index: number) => {
+    setActive(index);
+    onSelect && onSelect(index);
+  };
+
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick,
+  };
   return (
     <ul className={classes} style={style}>
-      {children}
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
     </ul>
   );
 };
