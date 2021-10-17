6813e810d7cce7ab800d34c339ead1b94de29f6c
Torvalds Du <torvalds@ekohe.com>
2021-10-17 21:05:51 +0800

    Display dropdown

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,4 +1,9 @@
-import React, { useContext, FunctionComponentElement } from 'react';
+import React, {
+  useState,
+  useContext,
+  FunctionComponentElement,
+  ReactEventHandler,
+} from 'react';
 import classNames from 'classnames';
 import { MenuContext } from './menu';
 import { MenuItemProps } from './menuItem';
@@ -14,11 +19,47 @@ const SubMenu: React.FC<SubMenuProps> = ({
   className,
   children,
 }) => {
+  const [menuOpen, setOpen] = useState<boolean>(false);
   const context = useContext(MenuContext);
   const classes = classNames('menu-item submenu-item', className, {
     'is-active': context.index === index,
   });
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
+
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggle);
+    }, 300);
+  };
+
+  const clickEvents =
+    context.mode === 'vertical'
+      ? {
+          onClick: handleClick,
+        }
+      : {};
+  const hoverEvents =
+    context.mode !== 'vertical'
+      ? {
+          onMouseEnter: (e: React.MouseEvent) => {
+            handleMouse(e, true);
+          },
+          onMouseLeave: (e: React.MouseEvent) => {
+            handleMouse(e, false);
+          },
+        }
+      : {};
+
   const renderChildren = () => {
+    const subMenuClasses = classNames('viking-submenu', {
+      'menu-opened': menuOpen,
+    });
     const childrenComponent = React.Children.map(children, (child, index) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>;
       if (childElement.type.displayName === 'MenuItem') {
@@ -27,11 +68,13 @@ const SubMenu: React.FC<SubMenuProps> = ({
         console.error('Warning: Submenu has a child which is not a MenuItem');
       }
     });
-    return <ul className={'viking-submenu'}>{childrenComponent}</ul>;
+    return <ul className={subMenuClasses}>{childrenComponent}</ul>;
   };
   return (
-    <li key={index} className={classes}>
-      <div className="submenu-title">{title}</div>
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" {...clickEvents}>
+        {title}
+      </div>
       {renderChildren()}
     </li>
   );
