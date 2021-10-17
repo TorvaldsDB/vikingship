9737e9c5828a67e34b6a1302249b6d827f53532f
Torvalds Du <torvalds@ekohe.com>
2021-10-17 18:41:57 +0800

    Display submenu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/subMenu';
 
 function App() {
   return (
@@ -12,11 +13,24 @@ function App() {
           onSelect={(index) => console.log(index)}
           mode="vertical"
         >
-          <MenuItem index={0}>cool link</MenuItem>
-          <MenuItem index={1} disabled>
-            cool link 2
-          </MenuItem>
-          <MenuItem index={2}>cool link 3</MenuItem>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem disabled>cool link 2</MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>dropdown 1</MenuItem>
+            <MenuItem>dropdown 2</MenuItem>
+            <MenuItem>dropdown 3</MenuItem>
+          </SubMenu>
+          <MenuItem>cool link 3</MenuItem>
+        </Menu>
+        <Menu defaultIndex={0} onSelect={(index) => console.log(index)}>
+          <MenuItem>cool link</MenuItem>
+          <MenuItem disabled>cool link 2</MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>dropdown 1</MenuItem>
+            <MenuItem>dropdown 2</MenuItem>
+            <MenuItem>dropdown 3</MenuItem>
+          </SubMenu>
+          <MenuItem>cool link 3</MenuItem>
         </Menu>
         <Button
           autoFocus
