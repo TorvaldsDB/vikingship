78245c3f85beaf7afcb6a7d1973f85a1dec9e6f8
Torvalds Du <torvalds@ekohe.com>
2021-10-24 20:03:07 +0800

    Icon CSS transform

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,12 +1,8 @@
-import React, {
-  useState,
-  useContext,
-  FunctionComponentElement,
-  ReactEventHandler,
-} from 'react';
+import React, { useState, useContext, FunctionComponentElement } from 'react';
 import classNames from 'classnames';
 import { MenuContext } from './menu';
 import { MenuItemProps } from './menuItem';
+import Icon from '../Icon/icon';
 
 export interface SubMenuProps {
   index?: string;
@@ -28,6 +24,8 @@ const SubMenu: React.FC<SubMenuProps> = ({
   const [menuOpen, setOpen] = useState<boolean>(isOpened);
   const classes = classNames('menu-item submenu-item', className, {
     'is-active': context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode === 'vertical',
   });
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -79,6 +77,7 @@ const SubMenu: React.FC<SubMenuProps> = ({
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
