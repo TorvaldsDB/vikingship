eafcc86a8715079a98395963a707c43caadeb1b2
Torvalds Du <torvalds@ekohe.com>
2021-10-24 12:07:26 +0800

    Icon style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import Icon from './components/Icon/icon';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
 import SubMenu from './components/Menu/subMenu';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(fas);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FontAwesomeIcon icon={faCoffee} size="10x" color="green" />
+        <Icon icon="coffee" theme="primary" size="10x" />
         <Menu
           defaultIndex="0"
           onSelect={(index) => alert(index)}
